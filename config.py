820b2830cb253d844cfbf59b7dfffe9bd58e27d5
Haeszm <99459243+Haeszm@users.noreply.github.com>
2023-11-29 03:18:06 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/anoexmusic/mahmoudaiden",
+    "https://github.com/Haeszm/rohb",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
